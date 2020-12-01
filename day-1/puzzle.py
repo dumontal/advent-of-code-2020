4ea5ec7fe7623b8ed7d650599ccb959d7b5b8391
Alexandre Dumont <ookachaka@protonmail.com>
2020-12-01 19:08:44 +0100

    Add day 1

---
+++ day-1/puzzle.py
@@ -0,0 +1,54 @@
+def read_file(name):
+    with open(name, "r") as file:
+        return [line.strip() for line in file]
+
+
+def find_pair(entries_set, target_sum):
+    for value in entries_set:
+        complementary_value = target_sum - value
+
+        if complementary_value in entries_set:
+            return value, complementary_value
+
+    return None
+
+
+def find_trio(entries_set, target_sum):
+    for value in entries_set:
+        sub_set = entries_set - { value }
+        sub_target_sum = target_sum - value
+        complementary_pair = find_pair(sub_set, sub_target_sum)
+
+        if complementary_pair is not None:
+            (second, third) = complementary_pair
+            return value, second, third
+
+    return None
+
+
+def main():
+    lines = read_file("input.txt")
+    entries = [int(line) for line in lines]
+
+    entries_set = set(entries)
+    assert len(entries) == len(entries_set), "Implementation requires no duplicates in the input"
+
+    pair = find_pair(entries_set, 2020)
+
+    if pair is None:
+        print("Pair not found")
+    else:
+        first, second = pair
+        print("Found pair {} x {} = {}".format(first, second, first * second))
+
+    trio = find_trio(entries_set, 2020)
+
+    if trio is None:
+        print("Trio not found")
+    else:
+        (first, second, third) = trio
+        print("Found trio {} x {} x {} = {}".format(first, second, third, first * second * third))
+
+
+if __name__ == '__main__':
+    main()
